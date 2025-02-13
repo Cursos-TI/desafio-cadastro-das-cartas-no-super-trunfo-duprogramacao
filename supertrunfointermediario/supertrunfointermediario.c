cd1b85512bd3bc4468d9394551fae071c0681a8d
duprogramacao <duabreu27@gmail.com>
2025-02-13 20:02:15 +0000

    commit tudo pronto

--- supertrunfointermediario/supertrunfointermediario.c
+++ supertrunfointermediario/supertrunfointermediario.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 int main(){
     //Passo 1 - O sistema pede ao usuário dados
-    char nome;
+    char letra;
+    char nomeestado[100];
     int populacao;
     int area;
     int pib;
     int turistico;
 
-    printf("(;´･ω･) DESAFIO CARTAS DE SUPER TRUNFO\n");
+    printf("(;´･ω･) DESAFIO CARTAS DE SUPER TRUNFO (;´･ω･)\n");
+
+    printf("Insira o caractere do estado: \n");
+    scanf("%c", &letra);
 
     printf("Insira o nome do estado: \n");
-    scanf("%s", &nome);
+    scanf("%s", nomeestado);
 
     printf("Insira a população do estado: \n");
     scanf("%d", &populacao);
@@ -24,7 +28,8 @@ int main(){
     printf("Insira a quantidade de pontos turísticos: \n");
     scanf("%d", &turistico);
 
-    printf("Nome: %s\n", nome);
+    printf("Caractere: %c\n", letra);
+    printf("Nome: %s\n", nomeestado);
     printf("População: %d\n", populacao);
     printf("Área: %d\n", area);
     printf("PIB: %d\n", pib);
@@ -32,8 +37,11 @@ int main(){
 
     //Cálculo de PIB Per Capta
     float pibpercapta = (float) pib / populacao;
-    printf("PIB Per Capta: %.2f", pibpercapta);
+    printf("PIB Per Capta: %.2f\n", pibpercapta);
 
+    //Cálculo de densidade populacional
+    float densidade = (float) populacao / area;
+    printf("Densidade populacional: %.2f\n", densidade);
 
 
 
