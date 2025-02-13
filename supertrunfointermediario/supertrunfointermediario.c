2fedd3ba040053e5db620f717ca5faefef58d58d
duprogramacao <duabreu27@gmail.com>
2025-02-13 19:59:00 +0000

    commit coloquei o cálculo de densidade populacional

--- supertrunfointermediario/supertrunfointermediario.c
+++ supertrunfointermediario/supertrunfointermediario.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 int main(){
     //Passo 1 - O sistema pede ao usuário dados
+    char nome;
     int populacao;
     int area;
     int pib;
     int turistico;
+
+    printf("(;´･ω･) DESAFIO CARTAS DE SUPER TRUNFO\n");
+
+    printf("Insira o nome do estado: \n");
+    scanf("%s", &nome);
 
     printf("Insira a população do estado: \n");
     scanf("%d", &populacao);
@@ -18,11 +24,13 @@ int main(){
     printf("Insira a quantidade de pontos turísticos: \n");
     scanf("%d", &turistico);
 
+    printf("Nome: %s\n", nome);
     printf("População: %d\n", populacao);
     printf("Área: %d\n", area);
     printf("PIB: %d\n", pib);
     printf("Pontos turistícos: %d\n", turistico);
 
+    //Cálculo de PIB Per Capta
     float pibpercapta = (float) pib / populacao;
     printf("PIB Per Capta: %.2f", pibpercapta);
 
